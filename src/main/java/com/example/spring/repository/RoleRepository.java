65f183ab441b416a5549179707bb8a157f8734f8
Lucas.M <mathieu.yahia-amar@etu.univ-amu.fr>
2024-02-04 19:35:24 +0100

    feat: create roles in the db when starting

--- src/main/java/com/example/spring/repository/RoleRepository.java
+++ src/main/java/com/example/spring/repository/RoleRepository.java
@@ -8,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 
     Role findByRoleName(RoleName roleName);
+
+    boolean existsByRoleName (RoleName roleName);
 }
