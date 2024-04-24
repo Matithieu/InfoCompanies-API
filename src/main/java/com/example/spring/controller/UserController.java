e0ddce33d41872b99163a55141c155597e33d245
Mathieu <mathieu.yahia-amar@etu.univ-amu.fr>
2024-04-24 22:53:41 +0200

    add update user endpoint

--- src/main/java/com/example/spring/controller/UserController.java
+++ src/main/java/com/example/spring/controller/UserController.java
@@ -2,8 +2,11 @@ package com.example.spring.controller;
 
 import com.example.spring.DTO.User;
 import com.example.spring.keycloakClient.UserResource;
+import jakarta.ws.rs.core.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 import static com.example.spring.security.JwtUtils.getClaimFromJwt;
 
@@ -20,5 +23,15 @@ public class UserController {
     public User getUser() {
         String email = getClaimFromJwt("email");
         return userResource.getUserByEmail(email);
+    }
+
+    @PutMapping("/update-user")
+    public void updateUser(@RequestBody User user) {
+        if(user.getEmail() != null) {
+            String email = getClaimFromJwt("email");
+            if(Objects.equals(user.getEmail(), email)) {
+                userResource.updateUser(user);
+            }
+        }
     }
 }
