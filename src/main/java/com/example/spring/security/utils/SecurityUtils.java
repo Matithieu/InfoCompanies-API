8bdba205f5fc4123ec87b067c04f3d58e4669323
Mathieu <mathieu.yahia-amar@etu.univ-amu.fr>
2024-05-23 00:24:40 +0200

    Fix headers

--- src/main/java/com/example/spring/security/utils/SecurityUtils.java
+++ src/main/java/com/example/spring/security/utils/SecurityUtils.java
@@ -6,18 +6,18 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 public class SecurityUtils {
 
+    // Find the corresponding header of nginx: proxy_set_header X-User $user;
     public static String parseEmailFromHeader() {
         ServletRequestAttributes sra = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         assert sra != null;
         HttpServletRequest request = sra.getRequest();
-        System.out.println(request.getHeader("X-Auth-Request-Email"));
-        return request.getHeader("X-Auth-Request-Email");
+        return request.getHeader("X-Email");
     }
 
     public static String parseUserFromHeader() {
         ServletRequestAttributes sra = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         assert sra != null;
         HttpServletRequest request = sra.getRequest();
-        return request.getHeader("X-Auth-Request-User");
+        return request.getHeader("X-User");
     }
 }
