fac75d26992ac497d4b58a19fdb093fc56b5d6c8
Mathieu Yahia-Amar <mathieu.yahiaamar@pelico.io>
2024-06-12 17:23:52 +0200

    fix csrf (POST, PUT)

--- src/main/java/com/example/spring/security/SecurityConfig.java
+++ src/main/java/com/example/spring/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -19,7 +20,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         //.requestMatchers(HttpMethod.GET, "api/v1/company/test").permitAll()
                         .anyRequest().permitAll())
+
                 .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
