d1be9783c52e8c82b97af617bde28eab7edc02c0
Matithieu <mathieu.yahia-amar@etu.univ-amu.fr>
2025-12-28 11:36:55 +0100

    feat: introduce streamed ai

---
+++ src/main/java/com/example/spring/app/llm/LLMAnswerDTO.java
@@ -0,0 +1,10 @@
+package com.example.spring.app.llm;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LLMAnswerDTO {
+    private String answer;
+}
--- src/main/java/com/example/spring/app/llm/LLMController.java
+++ src/main/java/com/example/spring/app/llm/LLMController.java
@@ -1,7 +1,10 @@
 package com.example.spring.app.llm;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 
 @CrossOrigin
 @RestController
@@ -14,11 +17,23 @@ public class LLMController {
         this.chatClient = chatClientBuilder.build();
     }
 
-    @GetMapping("/ai")
-    String generation(String userInput) {
-        return this.chatClient.prompt()
+    @GetMapping("/ask-ai")
+    LLMAnswerDTO generation(String userInput) {
+        LLMAnswerDTO llmAnswerDTO = new LLMAnswerDTO();
+        String response = this.chatClient.prompt()
                 .user(userInput)
                 .call()
                 .content();
+
+        llmAnswerDTO.setAnswer(response);
+        return llmAnswerDTO;
+    }
+
+    @GetMapping(value = "/stream-ai", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ChatResponse> streamGeneration(@RequestParam String userInput) {
+        return chatClient.prompt()
+                .user(userInput)
+                .stream()
+                .chatResponse();
     }
 }
